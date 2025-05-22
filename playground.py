474b1fa70bd2828298d6b0b8a040b2c3a9f20cb1
MR-5OBOT <yassir7m7@gmail.com>
2025-05-22 12:24:45 +0100

    data setup

--- playground.py
+++ playground.py
@@ -3,6 +3,13 @@ import numpy as np
 
 from helpers.utils import *
 
-input_file = "./data/NASDAQ-1H-10000.csv"
+# input_file = "EURUSD_M15.csv"
+# fix_csv_format(input_file, output_file="./data/EURUSD_M15.csv")
 
-fix_csv_format(input_file, output_file="NASDAQ-1H-10000.csv")
+file = pd.read_csv(
+    "./data/EURUSD_M15.csv",
+    header=None,
+    names=["Time", "Open", "High", "Low", "Close", "Volume"],
+)
+
+file.to_csv("EURUSD_M15.csv", index=False)
